eeff9f1ad9af74c35f0537479ceda26733c1b7e0
VuongNQ <vuong11a3@gmail.com>
2023-11-20 23:53:13 +0700

    update

--- remote/next.config.js
+++ remote/next.config.js
@@ -1,6 +1,34 @@
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
+const deps = require("./package.json").dependencies;
+require("dotenv").config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    compiler: {
+        styledComponents: true,
+        removeConsole: process.env.APP_ENV === "production",
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        const remotes = {
+        };
+        const federationConfig = {
+            name: "remote",
+            filename: "static/chunks/remoteEntry.js",
+            exposes: {
+                "./Remote": "./src/remote-entry.ts",
+            },
+            remotes,
+            shared: {
+            },
+            extraOptions: {
+                exposePages: true,
+            },
+        };
+        // console.log(federationConfig);
+        config.plugins.push(new NextFederationPlugin(federationConfig));
+        return config;
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
