7de59a4cfe5c8a76f230ba4c2595a2e837878675
VuongNQ <vuong11a3@gmail.com>
2023-11-20 23:38:51 +0700

    add remote

--- shell/next.config.js
+++ shell/next.config.js
@@ -1,6 +1,36 @@
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
+const deps = require("./package.json").dependencies;
+require("dotenv").config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  compiler: {
+    styledComponents: true,
+    removeConsole: process.env.APP_ENV === "production",
+  },
+  images: {
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    const remotes = {
+      nextBrio: `nextBrio@http://localhost:3001/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`,
+      // reactBrio: "reactBrio@http://localhost:3002/remoteEntry.js",
+    };
+    const federationConfig = {
+      name: "shell",
+      filename: "static/chunks/remoteEntry.js",
+      exposes: {
+      },
+      remotes,
+      shared: {},
+      extraOptions: {
+        exposePages: true,
+      },
+    };
+    // console.log(federationConfig);
+    config.plugins.push(new NextFederationPlugin(federationConfig));
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
